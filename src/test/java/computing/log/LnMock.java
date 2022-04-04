fd7757ae140abfafd8f82bd175e2edb40e321a1d
Евгений Макарьев <evgenij_makarev20021@mail.ru>
2022-04-04 20:55:06 +0300

    All tests to log

--- src/test/java/computing/log/LnMock.java
+++ src/test/java/computing/log/LnMock.java
@@ -33,13 +33,13 @@ public class LnMock {
     public static LogFiveBase getLogFiveBaseMock() {
         LogFiveBase logFiveBaseFunction = mock(LogFiveBase.class);
 
-        when(logFiveBaseFunction.calculate(Mockito.eq(1 * Math.PI / 6))).thenReturn(-0.93195);
-        when(logFiveBaseFunction.calculate(Mockito.eq(1 * Math.PI / 4))).thenReturn(-0.34809);
-        when(logFiveBaseFunction.calculate(Mockito.eq(1 * Math.PI / 3))).thenReturn(0.06649);
-        when(logFiveBaseFunction.calculate(Mockito.eq(2 * Math.PI / 3))).thenReturn(1.06643);
-        when(logFiveBaseFunction.calculate(Mockito.eq(3 * Math.PI / 4))).thenReturn(1.23585);
-        when(logFiveBaseFunction.calculate(Mockito.eq(5 * Math.PI / 6))).thenReturn(1.38662);
-        when(logFiveBaseFunction.calculate(Mockito.eq(1 * Math.PI / 2))).thenReturn(0.65137);
+        when(logFiveBaseFunction.calculate(Mockito.eq(1 * Math.PI / 6))).thenReturn(-0.40157);
+        when(logFiveBaseFunction.calculate(Mockito.eq(1 * Math.PI / 4))).thenReturn(-0.14999);
+        when(logFiveBaseFunction.calculate(Mockito.eq(1 * Math.PI / 3))).thenReturn(0.02865);
+        when(logFiveBaseFunction.calculate(Mockito.eq(2 * Math.PI / 3))).thenReturn(0.45951);
+        when(logFiveBaseFunction.calculate(Mockito.eq(3 * Math.PI / 4))).thenReturn(0.53252);
+        when(logFiveBaseFunction.calculate(Mockito.eq(5 * Math.PI / 6))).thenReturn(0.59748);
+        when(logFiveBaseFunction.calculate(Mockito.eq(1 * Math.PI / 2))).thenReturn(1.43065);
         when(logFiveBaseFunction.calculate(Mockito.eq(10.0))).thenReturn(3.32022);
 
         when(logFiveBaseFunction.calculate(Double.POSITIVE_INFINITY)).thenReturn(Double.POSITIVE_INFINITY);
@@ -52,14 +52,14 @@ public class LnMock {
     public static LogTwoBase getLogTwoBaseMock() {
         LogTwoBase logTwoBaseFunction = mock(LogTwoBase.class);
 
-        when(logTwoBaseFunction.calculate(Mockito.eq(1 * Math.PI / 6))).thenReturn(-0.40157);
-        when(logTwoBaseFunction.calculate(Mockito.eq(1 * Math.PI / 4))).thenReturn(-0.14999);
-        when(logTwoBaseFunction.calculate(Mockito.eq(1 * Math.PI / 3))).thenReturn(0.02865);
-        when(logTwoBaseFunction.calculate(Mockito.eq(2 * Math.PI / 3))).thenReturn(0.45951);
-        when(logTwoBaseFunction.calculate(Mockito.eq(3 * Math.PI / 4))).thenReturn(0.53252);
-        when(logTwoBaseFunction.calculate(Mockito.eq(5 * Math.PI / 6))).thenReturn(0.59748);
-        when(logTwoBaseFunction.calculate(Mockito.eq(1 * Math.PI / 2))).thenReturn(0.28067);
-        when(logTwoBaseFunction.calculate(Mockito.eq(10.0))).thenReturn(1.43065);
+        when(logTwoBaseFunction.calculate(Mockito.eq(1 * Math.PI / 6))).thenReturn(-0.93195);
+        when(logTwoBaseFunction.calculate(Mockito.eq(1 * Math.PI / 4))).thenReturn(-0.34809);
+        when(logTwoBaseFunction.calculate(Mockito.eq(1 * Math.PI / 3))).thenReturn(0.06649);
+        when(logTwoBaseFunction.calculate(Mockito.eq(2 * Math.PI / 3))).thenReturn(1.06643);
+        when(logTwoBaseFunction.calculate(Mockito.eq(3 * Math.PI / 4))).thenReturn(1.23585);
+        when(logTwoBaseFunction.calculate(Mockito.eq(5 * Math.PI / 6))).thenReturn(1.38662);
+        when(logTwoBaseFunction.calculate(Mockito.eq(1 * Math.PI / 2))).thenReturn(0.65137);
+        when(logTwoBaseFunction.calculate(Mockito.eq(10.0))).thenReturn(3.32022);
 
         when(logTwoBaseFunction.calculate(Double.POSITIVE_INFINITY)).thenReturn(Double.POSITIVE_INFINITY);
         when(logTwoBaseFunction.calculate(Double.NEGATIVE_INFINITY)).thenReturn(Double.NaN);
--- src/test/java/computing/log/LogTwoBaseTest.java
+++ src/test/java/computing/log/LogTwoBaseTest.java
@@ -1,7 +1,57 @@
 package computing.log;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-class LogTwoBaseTest {
+@DisplayName("LogTwoBase tests")
+public class LogTwoBaseTest {
+
+    private static final double DELTA = 0.05;
+    public static final double ACCURACY = 0.001;
+
+    private LogTwoBase logTwoBase;
+
+    @BeforeEach
+    void setup(){
+        Ln lnFunction = new Ln(ACCURACY);
+        this.logTwoBase = new LogTwoBase(ACCURACY, lnFunction);
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/computing/log/log2_test.csv")
+    @DisplayName("Test data from table")
+    public void tableValuesTest(double expected, double num, double den) {
+        double actual = logTwoBase.calculate(num * Math.PI / den);
+        assertEquals(expected, actual, DELTA);
+    }
+
+    @Test
+    @DisplayName("Test nan")
+    public void nanStubsTest() {
+        double expected = Double.NaN;
+        double actual = logTwoBase.calculate(Double.NaN);
+        assertEquals(expected, actual, DELTA);
+    }
+
+    @Test
+    @DisplayName("Test positive infinity")
+    public void positiveInfinityStubsTest() {
+        double expected = Double.POSITIVE_INFINITY;
+        double actual = logTwoBase.calculate(Double.POSITIVE_INFINITY);
+        assertEquals(expected, actual, DELTA);
+    }
+
+    @Test
+    @DisplayName("Test negative infinity")
+    public void negativeInfinityStubsTest() {
+        double expected = Double.NaN;
+        double actual = logTwoBase.calculate(Double.NEGATIVE_INFINITY);
+        assertEquals(expected, actual, DELTA);
+    }
 
 }
